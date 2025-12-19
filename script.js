51a1b4035d471e09054ee8b2b83f9b29a1bb65e8
Canbomm <canbomim@hotmail.com>
2025-12-19 13:26:07 -0300

    v1

---
+++ script.js
@@ -0,0 +1,64 @@
+let database = [];
+let seconds = 1; // Starts paused
+let timerInterval = null;
+let isRunning = false;
+
+// Loads database
+async function loadData() {
+  const response = await fetch('data.json');
+  database = await response.json();
+  console.log("Database loaded!");
+}
+
+// Get random search from data.json and copy it to the clipboard
+function randomSearch(resultDisplay) {
+  const randomIndex = Math.floor(Math.random() * database.length);
+  const randomItem = database[randomIndex];
+  resultDisplay.innerText = randomItem.search;
+  userClipboard(randomItem.search);
+}
+
+// Copy to user's clipboard
+function userClipboard(text) {
+  navigator.clipboard.writeText(text).then(() => {
+    console.log("Successfully copied to clipboard!");
+  }).catch(err => {
+    console.error("Failed to copy: ", err);
+  });
+}
+
+// Changes the suggestion after 8 seconds
+function startTimer() {
+  const timerDisplay = document.getElementById('timer');
+  const resultDisplay = document.getElementById('result');
+  const btn = document.getElementById('toggleBtn');
+
+  const runLogic = () => {
+    seconds--;
+    timerDisplay.innerText = "Timer: " + seconds;
+    if (seconds < 1 && database.length > 0) {
+      seconds = 8;
+      randomSearch(resultDisplay);
+
+      resultDisplay.classList.add('animate-pop-out');
+      setTimeout(() => {
+        resultDisplay.classList.remove('animate-pop-out');
+      }, 500);
+    }
+  };
+
+  // Pause/continue button
+  btn.addEventListener('click', () => {
+    if (isRunning) {
+      clearInterval(timerInterval);
+      btn.innerText = "Iniciar"; // Start
+    } else {
+      timerInterval = setInterval(runLogic, 1000);
+      btn.innerText = "Pausar"; // Pause
+    }
+    isRunning = !isRunning;
+  });
+}
+
+// Start everything when the page opens
+loadData().then(startTimer);
